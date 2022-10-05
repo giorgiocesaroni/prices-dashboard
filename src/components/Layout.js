f12852be1e56b74343acb531427080e1066cd93f
g.cesaroni <g.cesaroni@reply.it>
2022-10-06 00:02:43 +0200

    Product Name

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { SelectedProductAtom } from "../context/recoil/atoms";
 import { LineChart } from "../LineChart";
 import Inspector from "./Inspector";
 import { ProductsSidebar } from "./ProductsSidebar";
 
 export default function Layout() {
+  const selectedProduct = useRecoilState(SelectedProductAtom);
   return (
     <div className="layout">
-      <h2 className="page-title">Price Analysis</h2>
+      <h2 className="page-title">{selectedProduct ?? "Price Analysis"}</h2>
       <main>
         <div className="card">
           <LineChart />
